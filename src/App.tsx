ecc2b74a3d5456bc7a005ebd3dcabd8f184148db
Preethi G <preethig5122002@gmail.com>
2026-03-08 10:43:20 +0530

    cleanup

--- src/App.tsx
+++ src/App.tsx
@@ -49,8 +49,6 @@ function App() {
     return skills.filter((skill) => skill.category === selectedCategory);
   }, [selectedCategory]);
 
-  const year = new Date().getFullYear();
-
   return (
     <>
       <header className="site-header">
